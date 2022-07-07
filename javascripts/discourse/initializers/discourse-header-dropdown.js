255717d05b1edf13db31b737136c34f62fefa3b5
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-07 14:24:48 -0700

    fix the nestedness

--- javascripts/discourse/initializers/discourse-header-dropdown.js
+++ javascripts/discourse/initializers/discourse-header-dropdown.js
@@ -134,16 +134,15 @@ export default {
           
         });
 
+      const burgerMenuElements = [];
       api.decorateWidget("header-buttons:before", (helper) => {
         return helper.
-        h("input.nav__toggle", {
-          id: "nav__toggle",
-          type: "checkbox"
-        },
-          h("label.nav__toggle-label", {
-            for : "nav__toggle"
-          }, 
-            h("span")));
+        h("input.nav__toggle", {id: "nav__toggle",type: "checkbox"},
+          ("label.nav__toggle-label", 
+          {
+          for : "nav__toggle"
+        }, h("span"))
+        );
       });
 
       api.decorateWidget("home-logo:after",(helper) => {
