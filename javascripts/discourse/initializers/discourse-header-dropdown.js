1c021fbfd77453d25ee966ef84a7d831d93932a3
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-07-07 14:37:14 -0700

    fix the nestedness

--- javascripts/discourse/initializers/discourse-header-dropdown.js
+++ javascripts/discourse/initializers/discourse-header-dropdown.js
@@ -146,7 +146,7 @@ export default {
         )
   
         api.decorateWidget("header-buttons:before", (helper) => {
-          return helper.h(htmlArry);
+          return helper.h(htmlArray);
           
         });
 
